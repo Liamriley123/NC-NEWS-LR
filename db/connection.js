b5d3443e77ade6a5c244b8f784fb30bacb39bce1
liamriley123 <liamriley123@yahoo.co.uk>
2019-01-18 12:05:09 +0000

    prod connection obj corrected

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,13 @@
 // current environment
 const ENV = process.env.NODE_ENV || "development";
-
-const DATABASE_URL = process.env.DATABASE_URL;
 // knex module
 const knex = require("knex");
-console.log(DATABASE_URL);
 // config object for project
 const dbConfig =
   ENV === "production"
     ? {
         client: "pg",
-        connection: {
-          database: DATABASE_URL
-        }
+        connection: process.env.DATABASE_URL
       }
     : require("../knexfile")[ENV];
 
