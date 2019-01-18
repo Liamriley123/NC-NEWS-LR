a83d32be0c877017cb759eda5a483e11ebb71bca
liamriley123 <liamriley123@yahoo.co.uk>
2019-01-18 12:48:19 +0000

    heroku changes made for api

--- controllers/api.js
+++ controllers/api.js
@@ -1,7 +1,9 @@
 const path = require("path");
+const endpoints = require("../public/endpoints");
 
 exports.sendJSON = (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "endpoints.JSON"), null, err => {
-    if (err) next(err);
-  });
+  // res.sendFile(path.join(__dirname, "../", "endpoints.JSON"), null, err => {
+  //   if (err) next(err);
+  // });
+  res.json(endpoints);
 };
