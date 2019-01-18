ada6e2165b5c46da520dd421ff6cc2d05f651953
liamriley123 <liamriley123@yahoo.co.uk>
2019-01-18 13:29:18 +0000

    readMe completed

--- controllers/api.js
+++ controllers/api.js
@@ -2,8 +2,5 @@ const path = require("path");
 const endpoints = require("../endpoints");
 
 exports.sendJSON = (req, res, next) => {
-  // res.sendFile(path.join(__dirname, "../", "endpoints.JSON"), null, err => {
-  //   if (err) next(err);
-  // });
   res.json(endpoints);
 };
