7983c29875763a6efdf57dd682daad21e04264b7
liamriley123 <liamriley123@yahoo.co.uk>
2019-01-18 13:02:22 +0000

    api endpoint adapted

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const endpoints = require("../public/endpoints");
+const endpoints = require("../endpoints");
 
 exports.sendJSON = (req, res, next) => {
   // res.sendFile(path.join(__dirname, "../", "endpoints.JSON"), null, err => {
