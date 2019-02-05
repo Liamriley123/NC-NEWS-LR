c36e92d0a7a6a79df8d846aaf1fe5a00d9d0ad48
liamriley123 <liamriley123@yahoo.co.uk>
2019-02-05 13:39:16 +0000

    emojis changed

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,32 +1,38 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
-    avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url:
+      "https://www.cartoonify.de/wp-content/plugins/svg-avatars-generator/data/temp-avatars/svgA20527738010644447.png"
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg',
+    username: "grumpy19",
+    name: "Paul Grump",
+    avatar_url:
+      "https://www.cartoonify.de/wp-content/plugins/svg-avatars-generator/data/temp-avatars/svgA7769324602898835.png"
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
-    avatar_url: 'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    username: "happyamy2016",
+    name: "Amy Happy",
+    avatar_url:
+      "https://www.cartoonify.de/wp-content/plugins/svg-avatars-generator/data/temp-avatars/svgA030439465964565482.png"
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg',
+    username: "cooljmessy",
+    name: "Peter Messy",
+    avatar_url:
+      "https://www.cartoonify.de/wp-content/plugins/svg-avatars-generator/data/temp-avatars/svgA8826168541743133.png"
   },
   {
-    username: 'weegembump',
-    name: 'Gemma Bump',
-    avatar_url: 'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg',
+    username: "weegembump",
+    name: "Gemma Bump",
+    avatar_url:
+      "https://www.cartoonify.de/wp-content/plugins/svg-avatars-generator/data/temp-avatars/svgA702626713083143.png"
   },
   {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
-    avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
-  },
+    username: "jessjelly",
+    name: "Jess Jelly",
+    avatar_url:
+      "https://www.cartoonify.de/wp-content/plugins/svg-avatars-generator/data/temp-avatars/svgA882946042602367.png"
+  }
 ];
